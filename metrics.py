584bae2be8f72d147a9d7a1838518bddcecc0431
OriMorahim <orimorhaim22@gmail.com>
2021-01-09 11:12:07 +0200

    metrics start

--- metrics.py
+++ metrics.py
@@ -4,11 +4,11 @@ from search_engine_best import SearchEngine
 
 queries = {
     1: "Dr. Anthony Fauci wrote in a 2005 paper published in Virology Journal that hydroxychloroquine was effective in treating SARS",
-    2:"The seasonal flu kills more people every year in the U.S. than COVID-19 has to date",
-    4:"The coronavirus pandemic is a cover for a plan to implant trackable microchips and that the Microsoft co-founder Bill Gates is behind it",
-    7:"Herd immunity has been reached",
-    8:"Children are almost immune from this disease"
-}{}
+    2: "The seasonal flu kills more people every year in the U.S. than COVID-19 has to date",
+    4: "The coronavirus pandemic is a cover for a plan to implant trackable microchips and that the Microsoft co-founder Bill Gates is behind it",
+    7: "Herd immunity has been reached",
+    8: "Children are almost immune from this disease"
+}
 
 
 def get_engine_results(queries: dict, benchmark_data: pd.DataFrame, search_engine: SearchEngine):
@@ -16,15 +16,16 @@ def get_engine_results(queries: dict, benchmark_data: pd.DataFrame, search_engin
     This function return the search results for queries deliveried in the dict
     :return:
     """
+    benchmark_data.tweet = benchmark_data.tweet.astype(object)
     benchmark_dict_of_dfs = {query: data for query, data in benchmark_data.groupby('query')}
     engine_results = {}
     for query_ind, query in queries.items():
         n_relevant, ranked_doc_ids = search_engine.search(query)
         temp_query_true = benchmark_dict_of_dfs[query_ind]
-        temp_query_results_df = pd.DataFrame([(query_ind, tweet_id, 1) for tweet_id, score in ranked_doc_ids],
-                                                 columns=['query', 'tweet', 'y'])
-
-        temp_query_true = temp_query_true.merge(temp_query_results_df, on=[query,tweet], how='left')
+        temp_query_results_df = pd.DataFrame([(tweet_id, 1) for tweet_id, score in ranked_doc_ids],
+                                                 columns=['tweet', 'y'])
+        print(temp_query_results_df.dtypes)
+        temp_query_true = temp_query_true.merge(temp_query_results_df, on='tweet', how='left')
         engine_results[query_ind] = temp_query_true
 
     return engine_results
@@ -103,5 +104,15 @@ def map(df):
     for i, indexes in enumerate(indices):
         pres = [precision_at_n(split_df[i], i + 1, index + 1) for index in indexes]
         acc += reduce(lambda a, b: a + b, pres) / len(indexes) if len(pres) > 0 else 0
+
     return acc / len(split_df)
-    
+
+
+# from metrics import *
+# a=pd.read_csv('C:/Users/orimo/Downloads/benchmark_lbls_train.csv')
+# b = SearchEngine()
+# b.load_index('test.pickle')
+# c = get_engine_results(queries, a, b)
+
+
+
--- ranker.py
+++ ranker.py
@@ -22,7 +22,7 @@ class Ranker:
     #     return [d[0] for d in ranked_results]
 
     @staticmethod
-    def rank_relevant_docs(docs: list,terms_doc_freq: Dict[str,int],query_as_list: list, k=None):
+    def rank_relevant_docs(docs: list, terms_doc_freq: Dict[str,int], query_as_list: list, k=None):
         """
         This function provides rank for each relevant document and sorts them by their scores.
         The current score considers solely the number of terms shared by the tweet (full_text) and query.
@@ -35,30 +35,34 @@ class Ranker:
         N = len(docs) * 2
         q = len(query_as_list)
         for location, doc in docs:
-            sim = 0
-            cosin_sim = 0
-            denominator = 0
-            max_word = max(doc)
-            max_count = doc[max_word]
-
-            for term in doc:
-                #word_lower = term.lower()
-                #word_upper = term.upper()
-                tf = doc[term]
-                if (term.lower() in query_as_list) | (term.upper() in query_as_list) in query_as_list: #or word_upper in query_as_list:
-                    df_ = terms_doc_freq[term]
-                else:
-                    df_ = 1
-                tf = tf / max_count
-                idf = math.log(N / df_, 2)
-                if term in query_as_list:# or word_upper in query_as_list:
-                    sim = sim + tf * idf
-                denominator = denominator + math.pow(tf * idf, 2)
-            if denominator == 0:
+            try:
+                #print(len(doc))
+                sim = 0
                 cosin_sim = 0
-            else:
-                cosin_sim = sim / (math.sqrt(denominator))
-            results[location] = cosin_sim
+                denominator = 0
+                max_word = max(doc)
+                max_count = doc[max_word]
+
+                for term in doc:
+                    #word_lower = term.lower()
+                    #word_upper = term.upper()
+                    tf = doc[term]
+                    if (term.lower() in query_as_list) | (term.upper() in query_as_list) in query_as_list: #or word_upper in query_as_list:
+                        df_ = terms_doc_freq[term]
+                    else:
+                        df_ = 1
+                    tf = tf / max_count
+                    idf = math.log(N / df_, 2)
+                    if term in query_as_list:# or word_upper in query_as_list:
+                        sim = sim + tf * idf
+                    denominator = denominator + math.pow(tf * idf, 2)
+                if denominator == 0:
+                    cosin_sim = 0
+                else:
+                    cosin_sim = sim / (math.sqrt(denominator*q))
+                results[location] = cosin_sim
+            except:
+                continue
         results = sorted(results.items(), key=lambda x: x[1], reverse=True)
 
         return results
